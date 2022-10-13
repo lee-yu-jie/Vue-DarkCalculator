2520c03b1a2a20fcfa9166b40c05a1decab2e467
LI YU JIE <yujie@LIdeMacBook-Air.local>
2022-10-13 21:08:44 +0800

    功能完成

--- src/App.vue
+++ src/App.vue
@@ -4,32 +4,33 @@
       <div class="display">{{display}}</div>
       <div class="row">
         <button @click="clear" class="special-key">AC</button>
-        <button @click="concatNum('1')" class="special-key">+/-</button>
-        <button class="special-key">&#37;</button>
-        <button class="operator-key">&#247;</button>
+        <button @click="negative" class="special-key">+/-</button>
+        <button @click="percent" class="special-key">%</button>
+        <button @click="concatOp('/')" class="operator-key">÷</button>
       </div>
       <div class="row">
         <button @click="concatNum('7')" class="number-key">7</button>
         <button @click="concatNum('8')" class="number-key">8</button>
         <button @click="concatNum('9')" class="number-key">9</button>
-        <button class="operator-key">&#215;</button>
+        <button @click="concatOp('*')" class="operator-key">×</button>
       </div>
       <div class="row">
         <button @click="concatNum('4')" class="number-key">4</button>
         <button @click="concatNum('5')" class="number-key">5</button>
         <button @click="concatNum('6')" class="number-key">6</button>
-        <button class="operator-key">–</button>
+        <button @click="concatOp('-')" class="operator-key">–</button>
       </div>
       <div class="row">
         <button @click="concatNum('1')" class="number-key">1</button>
         <button @click="concatNum('2')" class="number-key">2</button>
         <button @click="concatNum('3')" class="number-key">3</button>
-        <button class="operator-key">+</button>
+        <button @click="concatOp('+')" class="operator-key">+</button>
       </div>
       <div class="row">
-        <button @click="concatNum('0')" class="number-key double-size">0</button>
+        <button @click="concatNum('0')" class="number-key">0</button>
+        <button @click="dele" class="number-key">></button>
         <button @click="addPoint('.')" class="number-key">.</button>
-        <button class="operator-key">=</button>
+        <button @click="evaluate" class="operator-key">=</button>
       </div>
     </div>
   </main>  
@@ -41,25 +42,49 @@ export default {
     setup() {
       const display = ref('0')
       const concatNum = (num) => {
-        display.value === 0 ? display.value = num : display.value += num
+        display.value === '0' ? display.value = num : display.value += num
+      }
+      const concatOp = (op) => {
+        display.value.search(/\D$/) === -1 && (display.value += op)
       }
       const clear = () => {
         display.value = '0'
       }
-      const addPoint = (point) => {
-        if(display.value.indexOf('.') === -1){
-          display.value += point
+      const dele = () => {
+        display.value = display.value.substring(0, display.value.length - 1)
+        display.value === '' && (display.value = '0')
+      }
+      const negative = () => {
+        if( display.value !== '0' && display.value.indexOf('-') === -1){
+          display.value = '-' + display.value
+        }else if(display.value.indexOf('-') !== -1){
+          display.value = display.value.replace('-','')
         }
       }
-
-
-
+      const percent = () => {
+        if(display.value.search(/\D/) === -1 || display.value.indexOf('.') !== -1){
+          display.value = String(display.value / 100)
+        }
+      }
+      const addPoint = (point) => {
+        if(display.value.indexOf('.') === -1 && display.value.search(/\D$/) === -1){ 
+          (display.value += point)
+        }
+      }
+      const evaluate = () => {
+        display.value = String(eval(display.value))
+      }
 
     return{ 
       display,
       concatNum,
       clear,
-      addPoint
+      addPoint,
+      concatOp,
+      dele,
+      negative,
+      percent,
+      evaluate
     }
   }
 }
@@ -81,7 +106,7 @@ main{
 .calc{
   color: $ft-color;
   font-size: 1.5em;
-  box-shadow: 0 0 100px rgb(78, 78, 78);
+  animation: shadow 5s linear infinite;
   .display{
     text-align: end;
     margin-bottom: 1em;
@@ -90,6 +115,7 @@ main{
     border-radius: 2em;
     background: $bgColor;
     box-shadow: $shadow, $inside-shadow;
+    word-break: break-all;
   }
   width: 300px;
   background: $bgColor;
@@ -111,14 +137,16 @@ main{
       font-size: 1em;
       cursor: pointer;
     }
-    button[class*='double-size']{
-      width: 140px;
-      border-radius: 2em;
-    }
     button:hover{
       color: $ft-color;
       box-shadow: $inside-shadow;
     }
+  }
+  @keyframes shadow {
+    0%{box-shadow: 0 0 100px rgb(78, 78, 78)}
+    33%{box-shadow: 0 0 50px rgb(78, 78, 78)}
+    66%{box-shadow: 0 0 110px rgb(111, 110, 110)}
+    100%{box-shadow: 0 0 90px rgb(78, 78, 78)}
   }
 }
 
